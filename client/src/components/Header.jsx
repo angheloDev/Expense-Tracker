e7d6408789cc842fba29a8ecf1f1426d0f67266e
angheloDev <120046334+angheloDev@users.noreply.github.com>
2025-09-21 02:41:55 +0800

    header logo changed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,13 @@
 const Header = ({ total }) => {
 	return (
-		<header className='w-full  text-white shadow-md'>
+		<header className='w-full text-white shadow-md'>
 			<div className={`${total >= 5500 ? 'bg-red-600' : 'bg-[#1E293B]'} `}>
 				<div className='flex justify-center items-center bg-gradient-to-t from-[#3E6097] to-[#1E293B] rounded-b-4xl'>
 					<div className='w-40 h-40 overflow-hidden'>
 						<img
-							src='/ET.png'
+							src='/NET.png'
 							alt='example'
-							className='w-full h-full object-cover scale-180'
+							className='w-full h-full object-cover scale-90'
 						/>
 					</div>
 				</div>
